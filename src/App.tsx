6f271eb7e9a25215138f5b705193e92b02264c6d
Alex Devero <deveroalex@gmail.com>
2022-09-02 10:52:40 +0200

    Implement useTransition hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
+
+const numbers= [...new Array(20000).keys()]
 
 function App() {
+  const [query, setQuery] = React.useState('')
+  const [text, setText] = React.useState('')
+  const [isPending, startTransition] = React.useTransition()
+
+  const handleInputChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+
+    startTransition(() => {
+      setQuery(e.target.value)
+    })
+  }, [])
+
+  const list = React.useMemo(() => {
+    return numbers.map((i, index) => {
+      return query
+          ? i.toString().startsWith(query)
+          && <p key={index}>{i}</p>
+          : <p key={index}>{i}</p>
+    })
+  }, [query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <input type="text" onChange={handleInputChange} value={text} />
+        <div>
+          {isPending ? <p style={{ marginTop: 15 }}>Loading...</p> : list}
+        </div>
       </header>
     </div>
   );
